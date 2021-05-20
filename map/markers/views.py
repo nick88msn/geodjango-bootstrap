afa19cccf0b20e7fa5d7c18557b5aff921c2954a
evilsocket <nick88msn@gmail.com>
2021-05-20 09:51:23 +0200

    added urls map view and static files

--- map/markers/views.py
+++ map/markers/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+# Adding TemplateView
+from django.views.generic.base import TemplateView
+
 # Create your views here.
+
+class MarkersMapView(TemplateView):
+    template_name = 'map.html'
